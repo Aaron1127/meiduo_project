edd16251e8565721c3963363db08ddd8950886ca
Aaron <aaronliao1127@gmail.com>
2020-04-23 14:00:58 +0800

    customize User model and do migration

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """自定義用戶模型類"""
+    mobile = models.CharField(max_length=10, unique=True, verbose_name='手機號碼')
+
+    class Meta:
+        db_table = 'tb_users'  # 自定義表名
+        verbose_name = '用戶'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -14,9 +14,9 @@ import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(sys.path)
+# print(sys.path)
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
-print(sys.path)
+# print(sys.path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -206,3 +206,5 @@ LOGGING = {
         },
     }
 }
+
+AUTH_USER_MODEL = 'users.User'
