e0fd32ff4646f21d4bf72eb07c90aaaad65982e4
Aaron <aaronliao1127@gmail.com>
2020-04-23 11:41:11 +0800

    upload register.html and set register router for apps.users

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class RegisterView(View):
+    """用戶註冊"""
+
+    def get(self, request):
+
+        return render(request, 'register.html')
+
